f59896840ca0d1d3adb70a388ac391544b46efed
Martin Sevrin <sevrinm@flip1.engr.oregonstate.edu>
2018-02-09 13:35:15 -0800

    hpp files populated

--- simulation.cpp
+++ simulation.cpp
@@ -1,1 +1,22 @@
+
+#include <iostream>
+#include <vector>
 #include "simulation.hpp"
+#include "critter.hpp"
+#include "utility.hpp"
+using std::cout;
+using std::endl;
+
+//constructor
+Simulation::Simulation() {}
+
+//destructor
+Simulation::~Simulation()
+{
+	//code
+}
+
+//misc.
+void Simulation::run()
+{
+}
--- simulation.hpp
+++ simulation.hpp
@@ -1,8 +1,31 @@
-#ifndef SIMULATION_HPP
-#define SIMULATION_Hpp
 
-class Simulation
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP	
+
+#include <vector>
+#include "critter.hpp"
+using std::vector;
+
+class Simulation 
 {
+	private:
+		char** board;
+		int maxSteps;
+		int maxRows;
+		int maxColumns;
+
+	public:
+		//constructors & destructors	
+		Simulation();
+		~Simulation();
+		
+		//misc.
+		void run();
+		void breedEveryone();
+		void Starve();
+		void newCritter(int row, int col);
+		void removeCritter(int row, int col);
+		void displayBoard();
 
 };
 
